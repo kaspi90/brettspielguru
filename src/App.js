b69d68b9e0bfc4aedfe2ff8ede6ba66984e5dbb5
Erik <foxmayz@gmx.de>
2022-06-10 12:31:21 +0200

    InstagramBlog, Access data from API

--- src/App.js
+++ src/App.js
@@ -1,58 +1,57 @@
-import './App.css';
-import React from 'react';
-import ResponsiveAppBar from './ResponsiveAppBar';
-import { createTheme } from '@mui/material/styles';
-import { ThemeProvider } from '@emotion/react';
-import Selectgame from './Selectgame';
-import DataTable from './DataTable';
-import Kartenhüllen from './Kartenhüllen';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { gameContext } from './GameContext';
-import Impressum from './Impressum.js';
-import Datenschutz from './Datenschutz.js';
-import { Box, width } from '@mui/system';
-import axios from 'axios';
-import { Button } from '@mui/material';
+import "./App.css";
+import React from "react";
+import ResponsiveAppBar from "./ResponsiveAppBar";
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@emotion/react";
+import Selectgame from "./Selectgame";
+import DataTable from "./DataTable";
+import Kartenhüllen from "./Kartenhüllen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { gameContext } from "./GameContext";
+import Impressum from "./Impressum.js";
+import Datenschutz from "./Datenschutz.js";
+import { Box, width } from "@mui/system";
+import { Button } from "@mui/material";
+import InstagramBlog from "./InstagramBlog";
 
 const theme = createTheme({
   palette: {
     primary: {
-      light: '#760019',
-      main: '#aa0124',
-      dark: '#bb334f',
+      light: "#760019",
+      main: "#aa0124",
+      dark: "#bb334f",
     },
     secondary: {
-      light: '#ab003c',
-      main: '#f50057',
-      dark: '#f73378',
+      light: "#ab003c",
+      main: "#f50057",
+      dark: "#f73378",
     },
   },
 });
 
 const Bundles = () => (
   <div>
-      <Selectgame />
-      <br />
-      <DataTable />
+    <Selectgame />
+    <br />
+    <DataTable />
   </div>
 );
 
 function App() {
-  const [game, setGame] = React.useState('');
-
- 
+  const [game, setGame] = React.useState("");
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <gameContext.Provider value={[game, setGame]}>
           <ResponsiveAppBar />
-          <Box maxWidth={'80%'} justify="center" m="auto" padding={2}>
-          <Routes>
-            <Route path="/" element={<Bundles />} />
-            <Route path="/kartenhuellen" element={<Kartenhüllen />} />
-            <Route path="/impressum" element={<Impressum />} />
-            <Route path="/datenschutz" element={<Datenschutz />} />
-          </Routes>
+          <Box maxWidth={"80%"} justify="center" m="auto" padding={2}>
+            <Routes>
+              <Route path="/" element={<Bundles />} />
+              <Route path="/kartenhuellen" element={<Kartenhüllen />} />
+              <Route path="/impressum" element={<Impressum />} />
+              <Route path="/datenschutz" element={<Datenschutz />} />
+              <Route path="/InstagramBlog" element={<InstagramBlog />} />
+            </Routes>
           </Box>
         </gameContext.Provider>
       </ThemeProvider>
--- src/InstagramBlog.js
+++ src/InstagramBlog.js
@@ -1,13 +1,38 @@
 import { Typography } from "@mui/material";
-import InstagramFeed from "react-ig-feed";
 import { Box } from "@mui/system";
 
 function InstagramBlog() {
-return (
-<Box>
-<Typography variant='h4' align="center">Unser Instagram-Feed</Typography>,
-<InstagramFeed token="IGQVJWcjdEWWdCT0dBWEI4SnlHVjRxb3VzODBfcW54OU9UUEpmYWtnemxPYnJCMHYtSmE5a25la3dfZAXJWd2FFd2F5YlowblBVQnZAmdzVtYlpFX2ZA4ZAXN6TGR0eTEyVUJMXzA3WldFWUQ3TE5sTnFjaQZDZD"  counter="24"/>
-</Box> 
-)
+  let data;
+
+  function accessInstagramFeed() {
+    const axios = require("axios");
+
+    axios({
+      method: "get",
+      url: "https://v1.nocodeapi.com/kaspi/instagram/KOTShLfOXSyGtqWm",
+      params: {},
+    })
+      .then(function (response) {
+        // handle success
+        console.log(response.data);
+        data = response.data;
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }
+
+  accessInstagramFeed();
+
+  console.log("test:" + data);
+
+  return (
+    <Box>
+      <Typography variant="h4" align="center">
+        Unser Instagram-Feed
+      </Typography>
+    </Box>
+  );
 }
 export default InstagramBlog;
