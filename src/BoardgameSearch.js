756ec8fde07be60a5dc415b8bf528a389b1daa41
Erik <foxmayz@gmx.de>
2022-07-01 13:54:22 +0200

    added setState to BoardgameSearch

--- src/BoardgameSearch.js
+++ src/BoardgameSearch.js
@@ -1,17 +1,20 @@
 import { Box } from "@mui/system";
 import { Grid } from "@mui/material";
 import { Typography } from "@mui/material";
-
+import * as React from "react";
 import { Item, classes } from "./DataTable";
 import { brettspiele } from "./DataTable";
+import { gameContext } from "./GameContext";
 
 function BoardgameSearch() {
+  const [game, setGame] = React.useContext(gameContext);
+
   let boardgames_grid = [];
   brettspiele.forEach((element) => {
     boardgames_grid.push(
       <Grid item xs={6} md={2}>
-        <Item>
-          <a href="#">
+        <Item style={{ minHeight: 260 }}>
+          <a href="./" onClick={setGame(element.name)}>
             <img src={element.boardgame_image}></img>
           </a>
           <Typography>{element.name}</Typography>
